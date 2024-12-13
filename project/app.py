1b9ee6f12467ce4c9dc7b12a2b0278b4b2991bfa
Miaoli Jiang <76165158+aoaow@users.noreply.github.com>
2024-12-13 18:23:58 +0800

    Update app.py

--- project/app.py
+++ project/app.py
@@ -62,3 +62,6 @@ def index():
 def handle_exception(e):
     print(f"Unhandled exception: {e}")
     return jsonify({"error": "Internal Server Error"}),
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=3000)
